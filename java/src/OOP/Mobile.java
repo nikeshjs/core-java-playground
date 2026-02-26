161cc7775455a9f4bd2a4961f57cd84a9c23c9d2
nikeshjs <nikeshjs123@gmail.com>
2026-02-26 21:45:43 +0545

    Started OOP on Falgun 14 2082

---
+++ java/src/OOP/Mobile.java
@@ -0,0 +1,18 @@
+package OOP;
+
+public class Mobile {
+    int ram;
+    String brand;
+    int storage;
+    String processor;
+
+    public void Output(){
+        System.out.println("New phone");
+    }
+}
+
+/* In Java, classes and main are kept in separate files (recommended).
+If in same package, no need to do anything. Otherwise, specify the package or simply import.
+Everything should be inside the class.
+Nothing exists outside the class. Everything of Java should be inside the class.
+ */
---
+++ java/src/OOP/main.java
@@ -0,0 +1,11 @@
+package OOP;
+
+public class main {
+    public static void main(String[] args) {
+        Mobile smartPhone = new Mobile();
+        smartPhone.brand = "Samsung";
+        smartPhone.Output();
+
+    }
+
+    }
